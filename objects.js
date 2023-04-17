f79d90dca9268d48846e2292c29b4341ed609597
NishanthD32 <130633141+NishanthD32@users.noreply.github.com>
2023-04-17 21:50:30 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-// javascript object is a collectionof named values having state and behaviar
+// javascript object is a collection of named values having state and behaviar
 // ex: person, car, pen, bike, etc..
  
 var bike = {make:"pulsar", model:"150", color:"white"};
